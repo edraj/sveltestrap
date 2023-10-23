741ae4b95c4f28db7a816ad44625ae0def639d06
splimter <11891837+splimter@users.noreply.github.com>
2023-10-23 11:32:04 +0100

    extending ui

--- src/AccordionHeader.svelte
+++ src/AccordionHeader.svelte
@@ -1,6 +1,7 @@
 <script>
   import classnames from './utils';
-  import Button from "./Button.svelte";
+  // import Button from "./Button.svelte";
+  import Icon from "./Icon.svelte";
   let className = '';
   export { className as class };
 
@@ -8,11 +9,10 @@
 </script>
 
 <h2 class="accordion-header" {...$$restProps}>
-  <div class="accordion-button collapsed d-flex" data-bs-toggle="collapse" data-bs-target="#item_1" aria-expanded="false" aria-controls="#item_1">
+  <div class="accordion-button collapsed d-flex" data-bs-toggle="collapse" on:click>
     <h5 class="content_title fw-bold"><slot /></h5>
     <div class="ms-auto">
-      <span class="fa fa-pencil-alt cercle-icons edit-icon" data-app-id="1" data-content-id="1" aria-hidden="true"></span>
-      <span class="far fa-trash-alt cercle-icons delete-icon" data-app-id="1" data-content-id="1" aria-hidden="true"></span>
+      <span class="far fa-trash-alt cercle-icons delete-icon" data-app-id="1" data-content-id="1" aria-hidden="true"><Icon name="globe2" /></span>
     </div>
   </div>
 <!--  <button type="button" class={classes} on:click>-->
