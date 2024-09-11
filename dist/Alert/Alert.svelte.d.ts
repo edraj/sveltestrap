7dfca3605a3fdd41f6977f2e1c6eeabc853670c3
splimter <11891837+splimter@users.noreply.github.com>
2024-09-11 22:26:55 +0100

    final fix.

--- dist/Alert/Alert.svelte.d.ts
+++ dist/Alert/Alert.svelte.d.ts
@@ -1,15 +1,34 @@
 /** @typedef {typeof __propDef.props}  AlertProps */
 /** @typedef {typeof __propDef.events}  AlertEvents */
 /** @typedef {typeof __propDef.slots}  AlertSlots */
-export default class Alert {
+export default class Alert extends SvelteComponent<{
+    [x: string]: any;
+    class?: string | undefined;
+    children?: any;
+    closeAriaLabel?: string | undefined;
+    closeClassName?: string | undefined;
+    color?: string | undefined;
+    dismissible?: boolean | undefined;
+    fade?: boolean | undefined;
+    heading?: string | undefined;
+    isOpen?: boolean | undefined;
+    toggle?: Function | undefined;
+    theme?: string | undefined;
+    transition?: object | undefined;
+}, {
+    [evt: string]: CustomEvent<any>;
+}, {
+    heading: {};
+    default: {};
+}> {
 }
 export type AlertProps = typeof __propDef.props;
 export type AlertEvents = typeof __propDef.events;
 export type AlertSlots = typeof __propDef.slots;
+import { SvelteComponent } from "svelte";
 declare const __propDef: {
     props: {
         [x: string]: any;
-        heading?: string | undefined;
         class?: string | undefined;
         children?: any;
         closeAriaLabel?: string | undefined;
@@ -17,6 +36,7 @@ declare const __propDef: {
         color?: string | undefined;
         dismissible?: boolean | undefined;
         fade?: boolean | undefined;
+        heading?: string | undefined;
         isOpen?: boolean | undefined;
         toggle?: Function | undefined;
         theme?: string | undefined;
